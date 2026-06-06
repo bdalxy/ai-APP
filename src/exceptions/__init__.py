ce75e0622168af62195db0ad70fa8184c5643e19
bdalxy <1617496242@qq.com>
2026-06-06 15:51:03 +0800

    重构: 清理仓库结构，新增 src 核心模块 (api_client, chat_engine, config, exceptions, memory init)

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,30 +1,4 @@
 # 自定义异常模块
-from src.exceptions.api_exceptions import (
-    APIContentFilterError,
-    APIException,
-    APIKeyError,
-    APIQuotaError,
-    APIRateLimitError,
-    APIServerError,
-    APITimeoutError,
-)
-from src.exceptions.memory_exceptions import (
-    MemoryException,
-    MemoryNotFoundError,
-    MemoryStorageError,
-)
-
-__all__ = [
-    # API 异常
-    "APIException",
-    "APIKeyError",
-    "APIQuotaError",
-    "APITimeoutError",
-    "APIRateLimitError",
-    "APIServerError",
-    "APIContentFilterError",
-    # 记忆异常
-    "MemoryException",
-    "MemoryNotFoundError",
-    "MemoryStorageError",
-]
+from src.exceptions.api_exceptions import (APIContentFilterError, APIException, APIKeyError, APIQuotaError, APIRateLimitError, APIServerError, APITimeoutError)
+from src.exceptions.memory_exceptions import (MemoryException, MemoryNotFoundError, MemoryStorageError)
+__all__ = ["APIException", "APIKeyError", "APIQuotaError", "APITimeoutError", "APIRateLimitError", "APIServerError", "APIContentFilterError", "MemoryException", "MemoryNotFoundError", "MemoryStorageError"]
